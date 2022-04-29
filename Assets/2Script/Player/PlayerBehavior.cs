62b9de72c12dea95a6cceff0e511d54f56f28427
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-04-29 14:51:46 +0900

    Player와 Weapon이 마우스 방향으로 바라보도록 수정

--- Assets/2Script/Player/PlayerBehavior.cs
+++ Assets/2Script/Player/PlayerBehavior.cs
@@ -7,7 +7,15 @@ public class PlayerBehavior : MonoBehaviour
     private Rigidbody2D rigid;
     private Animator anim;
     private SpriteRenderer sprite;
+    private Camera mainCamera;
 
+    [SerializeField] private List<GameObject> weapons;
+
+    private float hAxis;
+    private float vAxis;
+    
+
+    [SerializeField] private float size;
     [SerializeField] private int health;
     [SerializeField] private float moveSpeed;
     [SerializeField] private int ammo9mm;
@@ -21,6 +29,7 @@ public class PlayerBehavior : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        mainCamera = Camera.main;
 
         var inst = Instantiate(sprite.material);
         sprite.material = inst;
@@ -31,25 +40,48 @@ public class PlayerBehavior : MonoBehaviour
         sprite.material.SetColor("_PlayerColor", Color.magenta);
     }
 
+    private void Update()
+    {
+
+        Look();
+    }
+
     private void FixedUpdate()
     {
         Move();
     }
 
+    private void GetInput()
+    {
+        hAxis = Input.GetAxis("Horizontal");
+        vAxis = Input.GetAxis("Vertical");
+
+    }
+
     private void Move()
     {
-        var moveDir = Vector3.ClampMagnitude(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f), 1f);
+        var moveDir = Vector3.ClampMagnitude(new Vector3(hAxis, vAxis, 0f), 1f);
 
         anim.SetBool("isWalk", moveDir != Vector3.zero ? true : false);
 
-        var curFlipX = sprite.flipX;
-        sprite.flipX = (moveDir.x != 0) ? (moveDir.x < 0) : curFlipX;
-
         transform.position += moveDir * Time.deltaTime * moveSpeed;
+    }
+
+    private void Look()
+    {
+        var mPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        var oPosition = transform.position;
+
+        sprite.flipX = mPosition.x < oPosition.x;
     }
 
     public void ChangeColor(Color _color)
     {
         sprite.color= _color;
+    }
+
+    private void Swap()
+    { 
+    
     }
 }
---
+++ Assets/2Script/Player/PlayerWeapon.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    private Camera mainCamera;
+    
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        Look();
+    }
+
+    private void Look()
+    {
+        var mPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        var oPosition = transform.position;
+
+        var isLeft = mPosition.x < oPosition.x;
+        sprite.flipX = isLeft;
+
+        var direction = isLeft ? oPosition - mPosition : mPosition - oPosition;
+                
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            
+        if (angle < -80f || angle > 80f)
+        {
+            angle = Mathf.Clamp(angle, -80f, 80f);
+        }
+
+        var rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = rotation;
+
+        
+    }
+}
