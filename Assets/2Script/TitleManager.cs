86aff21670e31969a3a4d0966e8d80e7e956fe62
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-06-07 16:10:42 +0900

    1. Start 버튼 클릭 시 연출 추가 2. Title Image 애니메이션 수정

--- Assets/2Script/TitleManager.cs
+++ Assets/2Script/TitleManager.cs
@@ -1,31 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
 
 public class TitleManager : MonoBehaviour
 {
     [SerializeField] private GameObject title;
     [SerializeField] private float startShakeIntensity;
+
+    [SerializeField] private Button gameStartButton;
+
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+
     private void Start()
     {
         CameraShaking.SetCameraShake(startShakeIntensity);
     }
 
-    private void Update()
+    private IEnumerator GameStart()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            GameStart();
-        }
-    }
+        gameStartButton.GetComponent<Animator>().SetTrigger("FadeOut");
+        gameStartButton.enabled = false;
 
-    private void GameStart()
-    {
+        yield return new WaitForSeconds(0.5f);
+        
         title.GetComponent<Animator>().SetTrigger("FadeOut");
+
+        yield return new WaitForSeconds(0.5f);
+
+        CameraShaking.SetCameraShake(startShakeIntensity / 2f);
+        var timer = 0f;
+        var totaltime = 1f;
+        while (timer <= 1f)
+        {
+            virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(5f, 2.5f, timer / totaltime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
     }
 
     public void OnClickStartButton()
     {
-        GameStart();
+        StartCoroutine(GameStart());
     }
 }
