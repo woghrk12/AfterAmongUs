49baa96d4ba69e13abe7a4d1cce8dd0c70ad047b
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-06-09 16:41:59 +0900

    화면 Fade In/Out 기능 추가

--- Assets/2Script/TitleManager.cs
+++ Assets/2Script/TitleManager.cs
@@ -68,7 +68,7 @@ public class TitleManager : MonoBehaviour
 
         yield return new WaitForSeconds(2f);
         
-        var t_totalTime = 1f;
+        var t_totalTime = 0.5f;
         var t_timer = 0f;
         var t_position= startVirtualCamera.transform.position;
 
@@ -92,14 +92,24 @@ public class TitleManager : MonoBehaviour
 
         t_timer = 0f;
         background.gameObject.SetActive(true);
-        var t_color = background.color;
 
-        while (t_timer <= t_totalTime)
+        yield return ChangeBackground(1f);
+    }
+
+    private IEnumerator ChangeBackground(float p_time)
+    {
+        var t_timer = 0f;
+        var t_color = background.color;
+        var t_fadeIn = t_color.a == 1f;
+        while (t_timer <= p_time)
         {
-            t_color.a = Mathf.Lerp(0f, 1f, t_timer / t_totalTime);
+            t_color.a = Mathf.Lerp(t_fadeIn ? 1f : 0f, t_fadeIn ? 0f : 1f, t_timer / p_time);
             background.color = t_color;
             t_timer += Time.deltaTime;
             yield return null;
         }
+
+        t_color.a = t_fadeIn ? 0f : 1f;
+        background.color = t_color;
     }
 }
