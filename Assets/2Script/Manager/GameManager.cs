c84e1977ee0cee3e29cd746b7e62fb084ca8493c
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-06-20 13:41:48 +0900

    GameManager.cs 추가

---
+++ Assets/2Script/Manager/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	private static GameManager instance;
+	public static GameManager Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				var obj = FindObjectOfType<GameManager>();
+
+				if (obj != null)
+				{
+					instance = obj;
+				}
+				else
+				{
+					var newObj = new GameObject().AddComponent<GameManager>();
+					instance = newObj;
+				}
+			}
+			return instance;
+		}
+	}
+
+	private void Awake()
+	{
+		var objs = FindObjectsOfType<GameManager>();
+
+		if (objs.Length != 1)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		DontDestroyOnLoad(gameObject);
+	}
+}
+
