a3c30b44d5b2bfef5a887b18a0d528477b3eb644
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-09-19 14:47:06 +0900

    Character 이동 구현

---
+++ Assets/2Script/Character/CharacterMove.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 0f;
+    private Vector3 moveDir = Vector3.zero;
+
+    private bool isLeft = false;
+    public bool IsLeft 
+    {
+        private set 
+        {
+            isLeft = value;
+            transform.localScale = new Vector3(isLeft ? -1f : 1f, 1f, 1f);
+        }
+        get { return isLeft; }
+    }
+
+    private void Update()
+    {
+        InputDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void InputDirection()
+    { 
+        moveDir = Vector3.ClampMagnitude(new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f), 1f);
+    }
+
+    private void Move()
+    {
+        transform.position += moveDir * Time.deltaTime * moveSpeed;
+
+        if (moveDir.x != 0f) IsLeft = moveDir.x < 0f;
+    }
+}
