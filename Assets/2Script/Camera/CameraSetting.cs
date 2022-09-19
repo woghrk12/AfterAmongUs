0afbf15e6f0f24900b9d9b085d2166f8024055a4
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-09-19 18:10:41 +0900

    화면 해상도 1920 * 1080으로 고정

---
+++ Assets/2Script/Camera/CameraSetting.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSetting : MonoBehaviour
+{
+    [SerializeField] private float setWidth = 0;
+    [SerializeField] private float setHeight = 0;
+    private Camera mainCam = null;
+
+    private void Awake()
+    {
+        mainCam = Camera.main;
+
+        SetResolution();
+    }
+
+    private void SetResolution()
+    {
+        var t_rect = mainCam.rect;
+        var t_targetRate = setWidth / setHeight;
+        var t_screenRate = (float)Screen.width / Screen.height;
+
+        if (t_targetRate < t_screenRate) t_rect.width = t_targetRate / t_screenRate;
+        else t_rect.height = t_screenRate / t_targetRate;
+
+        t_rect.x = (1f - t_rect.width) * 0.5f; t_rect.y = (1f - t_rect.height) * 0.5f;
+
+        mainCam.rect = t_rect;
+    }
+}
