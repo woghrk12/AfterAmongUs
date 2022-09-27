5feeab05584eecbe1b4fff696c7e9829fb67c824
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-09-27 14:22:12 +0900

    InGame Scene 기본 세팅

---
+++ Assets/2Script/Manager/2InGame/InGameUIManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameUIManager : MonoBehaviour
+{
+    private void Start()
+    {
+        UIManager.FadeIn();
+    }
+
+ 
+}
