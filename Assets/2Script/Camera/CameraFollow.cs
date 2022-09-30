c2b5c8df69ba1379b1f9b1790a066e9b45adf927
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-09-30 20:03:54 +0900

    InGame Scene 카메라 범위 제한

--- Assets/2Script/Camera/CameraFollow.cs
+++ Assets/2Script/Camera/CameraFollow.cs
@@ -8,9 +8,25 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Vector3 offset = Vector3.zero;
     [SerializeField] private float followSpeed = 0f;
 
+    [SerializeField] private Vector2 minPos = Vector2.zero;
+    [SerializeField] private Vector2 maxPos = Vector2.zero;
+
     private void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        transform.position = Vector3.Lerp(transform.position, desiredPosition, followSpeed * Time.deltaTime);
+        var t_desiredPos = target.position + offset;
+        var t_rawPos = Vector3.Lerp(transform.position, t_desiredPos, followSpeed * Time.deltaTime);
+        transform.position = LimitArea(t_rawPos);
+    }
+
+    private Vector3 LimitArea(Vector3 p_cameraPos)
+    {
+        Vector3 t_pos = p_cameraPos;
+
+        if (t_pos.x < minPos.x) t_pos.x = minPos.x;
+        if (t_pos.x > maxPos.x) t_pos.x = maxPos.x;
+        if (t_pos.y < minPos.y) t_pos.y = minPos.y;
+        if (t_pos.y > maxPos.y) t_pos.y = maxPos.y;
+
+        return t_pos;
     }
 }
