8095a23aace25de40ff2e4d92d25a37b71e7e741
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-10-14 15:59:45 +0900

    HitEffect Light 추가

--- Assets/2Script/Weapon/HitEffect.cs
+++ Assets/2Script/Weapon/HitEffect.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class HitEffect : MonoBehaviour
 {
     [SerializeField] private ParticleSystem particle = null;
+    [SerializeField] private Light2D pointLight = null;
 
     private void OnEnable()
     {
@@ -14,7 +16,19 @@ public class HitEffect : MonoBehaviour
     private IEnumerator PlayParticle()
     {
         particle.Play();
-        yield return new WaitForSeconds(particle.main.duration);
+        yield return ControllLight(particle.main.duration);
         ObjectPoolingManager.ReturnObject(gameObject);
+    }
+
+    private IEnumerator ControllLight(float p_duration)
+    {
+        var t_timer = 0f;
+        
+        while (t_timer < p_duration)
+        {
+            pointLight.intensity = Mathf.Lerp(1f, 0f, t_timer / p_duration);
+            t_timer += Time.deltaTime;
+            yield return null;
+        }
     }
 }
