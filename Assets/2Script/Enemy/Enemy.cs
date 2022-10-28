07d7be0bf0b74de541296b08b6a1865de41d83e3
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-10-28 14:46:19 +0900

    Enemy 추적 기능 보완 (타겟팅 기능 추가)

--- Assets/2Script/Enemy/Enemy.cs
+++ Assets/2Script/Enemy/Enemy.cs
@@ -10,19 +10,27 @@ public class Enemy : MonoBehaviour
     [SerializeField] private Damagable hitController = null;
     [SerializeField] private Chasing chaseController = null;
     [SerializeField] private CharacterMove moveController = null;
+    [SerializeField] private CharacterRader targetController = null;
 
     private float chaseRange { get { return chaseController.ChaseRange; } }
-    private float attackRange = 0f;
+    private float attackRange = 1f;
 
     private Region curRegion = null;
     [SerializeField]private Transform gamePlayer = null;
     private Vector3 target { get { return chaseController.Target; } }
 
+    private bool isMove { set { anim.SetBool("isWalk", value); } }
+
+    private void Start()
+    {
+        targetController.SetRange(2f);
+    }
+
     private IEnumerator Chase()
     {
         StartCoroutine(ChaseRegion());
 
-        while (Utilities.CalculateDist(transform.position, gamePlayer.position) > chaseRange)
+        while (!targetController.Target)
         {
             Move();
             yield return null;
@@ -30,14 +38,15 @@ public class Enemy : MonoBehaviour
 
         chaseController.IsChasing = false;
 
-        StartCoroutine(ChasePlayer());
+        var t_target = targetController.Target;
+        StartCoroutine(ChasePlayer(t_target));
 
-        while (Utilities.CalculateDist(transform.position, gamePlayer.position) > attackRange)
+        while (Utilities.CalculateDist(transform.position, t_target.position) > attackRange || !targetController.Target)
         {
             Move();
             yield return null;
         }
-
+        isMove = false;
         chaseController.IsChasing = false;
     }
 
@@ -45,7 +54,7 @@ public class Enemy : MonoBehaviour
 
     private IEnumerator ChaseRegion() => chaseController.ChaseRegion(inGameManager.FindRegion(curRegion));
 
-    private IEnumerator ChasePlayer() => chaseController.ChaseTarget(gamePlayer);
+    private IEnumerator ChasePlayer(Transform p_target) => chaseController.ChaseTarget(p_target);
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
